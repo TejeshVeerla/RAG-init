12ce53057634f7be57bd1d4108b626d5e34c39e2
Tejesh Veerla <veerlatejesh@gmail.com>
2026-07-20 12:13:40 +0530

    add idf inverse document frequency to get value on how rare term is

--- cli/keyword_search_cli.py
+++ cli/keyword_search_cli.py
@@ -1,3 +1,4 @@
+import math
 import string
 import argparse
 import json
@@ -5,7 +6,9 @@ import io
 from nltk.stem import PorterStemmer
 import sys
 import pickle
+from collections import Counter
 
+#HELPER FUNCTIONS AND VARIABLES
 def remove_punctuations(current):
 
     punctuations = string.punctuation
@@ -21,15 +24,37 @@ for i in range(len(stopwords)):
     stopwords[i] = remove_punctuations(stopwords[i])
 stopwords = set(stopwords)
 
+stemmer = PorterStemmer()
+def tokenizeText(text:str,stop_words = stopwords):
+    nopunc = remove_punctuations(text)
+    split_text = nopunc.lower().split()
+    valid_token = []
+    for i in split_text:
+        if i not in stop_words:
+            valid_token.append(i)
+
+    filtered_tokens = []
+    for token in valid_token:
+        filtered_tokens.append(stemmer.stem(token))
+    return filtered_tokens
+
+def tokenize_single_term(text:str):
+    token = tokenizeText(text)
+    if len(token) != 1:
+        raise ValueError("Expected only one token")
+    return "".join(token)
 
 class InvertedIndex():
     
     def __init__(self):
         self.index = {}
         self.docmap = {}
+        self.term_frequencies = {}
 
     def  __add_document(self, doc_id, text):
+        
         tokens = tokenizeText(text)
+        self.term_frequencies[doc_id] = Counter(tokens)
         for token in tokens:
             if token not in self.index:
                 self.index[token] = set()
@@ -60,6 +85,8 @@ class InvertedIndex():
         with open("cache/docmap.pkl","wb") as docmap_file:
             pickle.dump(self.docmap,docmap_file)
 
+        with open("cache/term_frequencies.pkl","wb") as term_frequencies_file:
+            pickle.dump(self.term_frequencies,term_frequencies_file)
 
     def load(self):
         with open("cache/index.pkl", "rb") as index_file:
@@ -69,19 +96,15 @@ class InvertedIndex():
             self.docmap = pickle.load(docmap_file)
 
 
-stemmer = PorterStemmer()
-def tokenizeText(text:str,stop_words = stopwords):
-    nopunc = remove_punctuations(text)
-    split_text = nopunc.lower().split()
-    valid_token = []
-    for i in split_text:
-        if i not in stop_words:
-            valid_token.append(i)
+        with open("cache/term_frequencies.pkl", "rb") as term_frequencies_file:
+            self.term_frequencies = pickle.load(term_frequencies_file)
 
-    filtered_tokens = []
-    for token in valid_token:
-        filtered_tokens.append(stemmer.stem(token))
-    return filtered_tokens
+    def get_tf(self,doc_id,term):
+        if doc_id not in self.term_frequencies:
+            return 0
+        if term not in self.term_frequencies[doc_id]:
+            return 0
+        return (self.term_frequencies[doc_id][term])
 
 
 
@@ -89,7 +112,8 @@ def build_command():
     inverted_index = InvertedIndex()
     inverted_index.Build()
     inverted_index.save()
-    # print(f"First document for token 'merida' = {inverted_index.get_documents('merida')[0]}")
+    # print(inverted_index.term_frequencies[1])
+    # print(f"First document for token 'merida' = {inverted_index.get_tf(1,'maya')}")
 
 
 def main() -> None:
@@ -99,6 +123,14 @@ def main() -> None:
     search_parser = subparsers.add_parser("search", help="Search movies using keywords")
     search_parser.add_argument("query", type=str, help="Search query")
     subparsers.add_parser("build", help="Build the inverted index and save it to disk")
+    
+    tf_parser = subparsers.add_parser("tf",help="add doc id and term to get term frequency")
+    tf_parser.add_argument("doc_id",type=int, help = "Enter the document id you want to check the frequency from:")
+    tf_parser.add_argument("term",type=str,help="add the term to check the frequency")
+    
+    idf_parser = subparsers.add_parser("idf",help="To retrieve information about Inverse document frequency of a term")
+    idf_parser.add_argument("term",type=str,help="add the term to check idf")
+
     args = parser.parse_args()
     
 
@@ -136,7 +168,30 @@ def main() -> None:
             # Print the final resulting document details cleanly
             for i, doc_id in enumerate(sorted_doc_ids):
                 print(f"{i+1}. [ID: {doc_id}] {InvIndex.docmap[doc_id]['title']}")
+        case "tf":
+            doc_id = args.doc_id
+            term = args.term
+            term = tokenize_single_term(term)
+            invIndex = InvertedIndex()
+            
+            try: 
+                invIndex.load()
+            except FileNotFoundError:
+                print("Error: File not Found, Try using build command first ")
 
+            print(invIndex.get_tf(doc_id,term))
+
+        case "idf":
+            invIndex = InvertedIndex()
+            try: 
+                invIndex.load()
+            except FileNotFoundError:
+                print("Error: File not Found, Try using build command first ")
+
+            term = args.term
+            term = tokenize_single_term(term)
+            idf_value = math.log((len(invIndex.docmap)+1)/(len(invIndex.index[term])+1)) 
+            print(f"Inverse document frequency of '{args.term}': {idf_value:.2f}")
         case _:
             parser.print_help()
 
